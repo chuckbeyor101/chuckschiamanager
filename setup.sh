7e4ea9a7955437755011a9cb5d5f426402f6b850
Chuck <chuckbeyor101@hotmail.com>
2021-06-02 13:26:40 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@
 echo "-----Install Chia and add keys-----"
 git clone https://github.com/Chia-Network/chia-blockchain.git ~/
 bash ~/chia-blockchain/install.sh
+cd ~/chia-blockchain/
 . ./activate
 chia init
 chia keys add -f keys.txt
