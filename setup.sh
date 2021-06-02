119d9f3119afe4cbeb80755e871abfd4998da17d
Chuck <chuckbeyor101@hotmail.com>
2021-06-02 11:13:53 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#change Permissions (Comment out these 2 lines for higher security)
+sudo sed -i 's/ALL=(ALL:ALL) ALL/ALL=(ALL:ALL) NOPASSWD:ALL/g' /etc/sudoers
+sudo sed -i 's/ALL=(ALL) ALL/ALL=(ALL) NOPASSWD:ALL/g' /etc/sudoers
+
+#Update System
+sudo apt update
+sudo apt upgrade -y
+sudo apt install git -y
+
+#Install iftop to monitor network connections
+sudo apt-get install iftop -y
+
+#Install SSH
+sudo apt install openssh-server
+
+#Install Samba
+sudo apt install samba -y
+
+#Install Chia and add keys
+git clone https://github.com/Chia-Network/chia-blockchain.git -b latest --recurse-submodules
+cd /home/$USERNAME/chia-blockchain
+sh install.sh
+. ./activate
+chia init
+chia keys add -f /home/$USERNAME/chiamanager/keys.txt
