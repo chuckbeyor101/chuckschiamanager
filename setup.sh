aa9b6e66bee0f14087e7a1f2aab84fe2d4787a44
Chuck <chuckbeyor101@hotmail.com>
2021-06-02 13:17:35 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,30 +1,10 @@
 #!/bin/bash
 
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-cd "$parent_path"
-
-echo "change Permissions (Comment out these 2 lines for higher security)"
-sudo sed -i 's/ALL=(ALL:ALL) ALL/ALL=(ALL:ALL) NOPASSWD:ALL/g' /etc/sudoers
-sudo sed -i 's/ALL=(ALL) ALL/ALL=(ALL) NOPASSWD:ALL/g' /etc/sudoers
-
-echo "-----Update System------"
-sudo apt update
-sudo apt upgrade -y
-sudo apt install git -y
-
-echo "-----Install iftop to monitor network connections-----"
-sudo apt-get install iftop -y
-
-echo "-----Install SSH-----"
-sudo apt install openssh-server
-
-echo "-----Install Samba-----"
-sudo apt -y install samba
 
 echo "-----Install Chia and add keys-----"
-git clone https://github.com/Chia-Network/chia-blockchain.git ~/
-cd ~/chia-blockchain
+git clone https://github.com/Chia-Network/chia-blockchain.git 
+cd /chia-blockchain
 bash install.sh
 . ./activate
 chia init
