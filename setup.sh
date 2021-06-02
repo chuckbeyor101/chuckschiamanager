b9de5fe193c734f53a9f323dfa537d9730b0b9c7
Chuck <chuckbeyor101@hotmail.com>
2021-06-02 12:28:28 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,6 +18,7 @@ sudo apt -y install samba
 
 echo "-----Install Chia and add keys-----"
 git clone https://github.com/Chia-Network/chia-blockchain.git ~/chia-blockchain
+cd ~/chia-blockchain
 bash install.sh
 . ./activate
 chia init
